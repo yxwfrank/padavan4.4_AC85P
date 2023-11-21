0679c220416b4e442e29f5b5c3cf1325df579ba2
frankyxw <107748433+frankyxw@users.noreply.github.com>
2023-11-21 22:45:19 +0800

    Update RT-AC85P board files

--- trunk/configs/boards/RT-AC85P/board.h
+++ trunk/configs/boards/RT-AC85P/board.h
@@ -8,20 +8,6 @@
 #define BOARD_MODEL_URL	"https://www.asus.com.cn/Networking-IoT-Servers/WiFi-Routers/ASUS-WiFi-Routers/RT-AC85P/"
 #define BOARD_BOOT_TIME	30
 #define BOARD_FLASH_TIME	120
-#undef  BOARD_GPIO_BTN_FN1	
-#define BOARD_GPIO_BTN_RESET	3
-#define BOARD_GPIO_BTN_WPS	6
-#undef  BOARD_GPIO_LED_ALL
-#undef  BOARD_GPIO_LED_WIFI
-#define  BOARD_GPIO_LED_SW2G    10
-#define  BOARD_GPIO_LED_SW5G    8
-#define  BOARD_GPIO_LED_POWER	 4
-#undef  BOARD_GPIO_LED_LAN
-#undef BOARD_GPIO_LED_WAN	
-#undef BOARD_GPIO_LED_USB  
-#undef BOARD_GPIO_LED_USB2 
-#undef BOARD_GPIO_LED_ROUTER
-#undef  BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX  4
 #define BOARD_NUM_ANT_5G_RX  4
